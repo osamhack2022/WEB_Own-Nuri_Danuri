1a6ef6b387b32f3ec4b4a08f557be17d4956f0de
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-05 14:39:28 +0000

    Page Setup tmp1

--- web_fe/src/App.tsx
+++ web_fe/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+
+
+import Main_page from './pages/Main_page';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-red-300'>Test Change</h1>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<Main_page />}/>
+
+          <Route path="/&" element={<NotFound_page />}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
