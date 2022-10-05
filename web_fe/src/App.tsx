1880464fa5083c90a461883509973593f8f51ad9
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-05 14:14:41 +0000

    test commit

--- web_fe/src/App.tsx
+++ web_fe/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-red-300'>Hello World!</h1>
+      <h1 className='bg-red-300'>Test Change</h1>
     </div>
   );
 }
