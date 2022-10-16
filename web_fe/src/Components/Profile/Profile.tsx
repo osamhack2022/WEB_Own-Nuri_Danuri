2b1846c61d6358a50129a444acc79e45043fade6
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-16 09:03:34 +0000

    10/14 publishing #3

--- web_fe/src/Components/Profile/Profile.tsx
+++ web_fe/src/Components/Profile/Profile.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import profile from '../../images/profile-tmp.png';
 
-const Profile : React.FC = () => {
-    <div className='flex flex-col'>
-        
-    </div>
+const Profile: React.FC = () => {
+    return (
+        <div className='flex flex-col text-center'>
+            <div id='ProfileFrame' className='rounded-full bg-white mb-4'>
+                <img src={profile} alt='profile image' />
+            </div>
+            <p>병장 송경호</p>
+        </div>
+    );
 }
 
 export default Profile;
--- web_fe/src/Pages/Dashboard/Sidebar/Sidebar.tsx
+++ web_fe/src/Pages/Dashboard/Sidebar/Sidebar.tsx
@@ -2,21 +2,22 @@ import * as React from 'react';
 import './SidebarStyle.css'
 import { NavLink } from 'react-router-dom';
 import { HiMenu, HiOutlineUser, HiOutlineGift, HiOutlineClipboard, HiOutlineSpeakerphone } from 'react-icons/hi';
+import Profile from '../../../Components/Profile/Profile';
 
 const Sidebar: React.FC = () => {
     return (
         <div id="SidebarContainer" className='h-full bg-yellow-300'>
-            <button><HiMenu /></button>
+            <button><HiMenu className='text-white'/></button>
 
-            <div className='flex flex-col'>
-                <div id="SideProfile" className='bg-pink-500'>
-                    
+            <div className='flex flex-col mt-8 h-2/3 justify-between items-center text-white'>
+                <div id="SideProfile" className='w-24 text-sm'>
+                    <Profile/>
                 </div>
 
-                <NavLink className='bg-pink-300' to='/dashboard/userinfo'><HiOutlineUser /></NavLink>
-                <NavLink className='bg-pink-300' to='/dashboard/mostbenefits'><HiOutlineGift /></NavLink>
-                <NavLink className='bg-pink-300' to='/dashboard/benefitlist'><HiOutlineClipboard /></NavLink>
-                <NavLink className='bg-pink-300' to='/dashboard/hosting'><HiOutlineSpeakerphone /></NavLink>
+                <NavLink className='bg-pink-300' to='/dashboard/userinfo'><HiOutlineUser className='text-white'/></NavLink>
+                <NavLink className='bg-pink-300' to='/dashboard/mostbenefits'><HiOutlineGift className='text-white'/></NavLink>
+                <NavLink className='bg-pink-300' to='/dashboard/benefitlist'><HiOutlineClipboard className='text-white'/></NavLink>
+                <NavLink className='bg-pink-300' to='/dashboard/hosting'><HiOutlineSpeakerphone className='text-white'/></NavLink>
             </div>
         </div>
     );
