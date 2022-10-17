9670d5cb0a7d8da6140104e41335bf738ac38909
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-17 05:22:22 +0000

    10/17 publishing #2

--- web_fe/src/Pages/Dashboard/MostBenefit/BenefitCard/BenefitCard.tsx
+++ web_fe/src/Pages/Dashboard/MostBenefit/BenefitCard/BenefitCard.tsx
@@ -8,7 +8,7 @@ interface props {
 const BenefitCard: React.FC<props> = ({ cardID }) => {
     if (cardID) {
         return (
-            <div id={'card' + cardID} className='w-20 h-20 rounded-md' />
+            <button className='block'><div id={'card' + cardID} className='w-20 h-20 duration-500 ease-linear rounded-md opacity-50 hover:w-40 hover:h-40 hover:opacity-100' /></button>
         )
     }
     else
