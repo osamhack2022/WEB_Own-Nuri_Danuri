5d845f6a121ff3a6bf5339ba2813d807c8ff68a6
이찬호 <52401201+ldhch2@users.noreply.github.com>
2022-10-17 03:29:57 +0000

    ㅁㅁ

--- web_fe/setupProxy.js
+++ web_fe/setupProxy.js
@@ -9,3 +9,36 @@ module.exports = function (app) {
         })
     );
 };
+
+
+
+// //ㅁ server/router/test.ts
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", (req: any, res: any) => {
+//     res.send({ test : "hi"});
+// })
+
+// module.exports = router;
+
+
+
+// //ㅁ client/src/App.tsx
+// import React, { useEffect } from 'react';
+// import axios from 'axios';
+// import './App.css';
+
+// function App() {
+//   const callApi = async () => {
+//     axios.get("/api").then((res) => console.log(res.data.test));
+//   }
+
+//   useEffect(() => {
+//     callApi();
+//   }, []);
+
+//   return <div>테스트</div>
+// }
+
+// export default App;
