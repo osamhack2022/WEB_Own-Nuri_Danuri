0a91c077ed4b858c0e70834148908d5790f06a2b
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-18 09:07:05 +0000

    tmp

--- web_fe/src/Pages/Dashboard/DashboardPage.tsx
+++ web_fe/src/Pages/Dashboard/DashboardPage.tsx
@@ -6,16 +6,19 @@ import useSidebar from './Sidebar/useSidebar';
 import { HiMenu } from 'react-icons/hi'
 
 const DashboardPage: React.FC = () => {
-    const { isShowed, toggleShowed } = useSidebar();
+    const { isShowed, toggleShowed, boardState, toggleState, getThemeColor } = useSidebar();
+    const themeColor : string = getThemeColor();
 
     return (
-        <div id='DashContainer' className='overflow-hidden relative'>
+        <div id='DashContainer' className='ease-linear duration-300 overflow-hidden relative' style={
+            { backgroundColor : themeColor }
+        }>
 
             <div id='DashContent' className='overflow-hidden flex absolute'>
                 
                 <div id="DashSidebar" className={`${isShowed ? 'slideOut' : 'slideIn'}`}>
                     <button onClick={toggleShowed} className='block m-4'><HiMenu className='text-white'/></button>
-                    <Sidebar isShowed={isShowed} />
+                    <Sidebar isShowed={isShowed} toggleState={toggleState} />
                 </div>
 
                 <div id='DashMainboard' className={`${isShowed ? 'slideOut' : 'slideIn'}`}>
--- web_fe/src/Pages/Dashboard/Sidebar/Sidebar.tsx
+++ web_fe/src/Pages/Dashboard/Sidebar/Sidebar.tsx
@@ -3,24 +3,33 @@ import './SidebarStyle.css'
 
 import { NavLink } from 'react-router-dom';
 import { HiOutlineUser, HiOutlineGift, HiOutlineClipboard, HiOutlineSpeakerphone } from 'react-icons/hi';
+import { BoardState } from './useSidebar';
 import Profile from '../../../Components/Profile/Profile';
 
 interface Props {
-    isShowed : boolean
+    isShowed: boolean
+    toggleState: (state: BoardState) => void;
 }
 
-const Sidebar: React.FC<Props> = ({ isShowed }) => {
+const Sidebar: React.FC<Props> = ({ isShowed, toggleState }) => {
     return (
         <div id="SidebarContainer" className='h-full'>
             <div className='flex flex-col mt-8 h-fit justify-between items-center text-white'>
                 <div id="SideProfile" className='mb-8 w-24 text-sm'>
-                    {isShowed ? <Profile/> : null}
+                    {isShowed ? <Profile /> : null}
                 </div>
 
-                <NavLink className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/userinfo'><HiOutlineUser className='text-white'/></NavLink>
-                <NavLink className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/mostbenefits'><HiOutlineGift className='text-white'/></NavLink>
-                <NavLink className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/benefitlist'><HiOutlineClipboard className='text-white'/></NavLink>
-                <NavLink className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/hosting'><HiOutlineSpeakerphone className='text-white'/></NavLink>
+                <NavLink onClick={()=>toggleState(BoardState.UserInfo)}
+                    className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/userinfo'><HiOutlineUser className='text-white' /></NavLink>
+                
+                <NavLink onClick={()=>toggleState(BoardState.MostBenefits)}
+                    className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/mostbenefits'><HiOutlineGift className='text-white' /></NavLink>
+                
+                <NavLink onClick={()=>toggleState(BoardState.BenefitList)}
+                    className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/benefitlist'><HiOutlineClipboard className='text-white' /></NavLink>
+                
+                <NavLink onClick={()=>toggleState(BoardState.Hosting)}
+                className='p-1 mb-4 border-2 border-white rounded-full' to='/dashboard/hosting'><HiOutlineSpeakerphone className='text-white' /></NavLink>
             </div>
         </div>
     );
