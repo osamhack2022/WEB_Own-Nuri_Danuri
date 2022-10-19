4e477ac32b1e34c5d49251fedc2aef57410afc9d
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-19 14:48:35 +0000

    jsonserver testing

--- web_fe/src/Pages/Dashboard/UserInfo/UserInfo.tsx
+++ web_fe/src/Pages/Dashboard/UserInfo/UserInfo.tsx
@@ -3,7 +3,12 @@ import './UserInfoStyle.css'
 import profile_tmp from '../../../images/profile-tmp.png'
 import { HiOutlinePencilSquare } from 'react-icons/hi2';
 
+import useUserInfo, { User } from './useUserInfo';
+
 const Userinfo: React.FC = () => {
+    const { userSet } = useUserInfo('http://localhost:3001/UserInfo');
+    const user: User = userSet[0];
+
     return (
         <div id='UserInfoContainer' className='flex justify-around items-center w-full h-full bg-white'>
             <div id='UserContent' className='flex justify-around items-center w-full h-5/6'>
@@ -12,17 +17,17 @@ const Userinfo: React.FC = () => {
 
                     <div id='UserFixInfo' className='flex flex-col h-2/3 justify-around'>
 
-                        <input id='milID' type='text' value='21-76028034' />
-                        <input id='name' type='text' value='송경호' />
-                        <input id='armyunit' type='text' value='1군단 101정보통신단 운용대대' />
+                        <input id='milID' type='text' value={user.id} />
+                        <input id='name' type='text' value={user.name} />
+                        <input id='armyunit' type='text' value={user.armyunit} />
 
                     </div>
 
                     <div id='UserModifyInfo' className='flex flex-col h-2/3 justify-around'>
 
-                        <input id='pw' placeholder='비밀번호' type='password' />
-                        <input id='nickname' placeholder='별명' type='text' />
-                        <select id='class'>
+                        <input id='pw' placeholder='비밀번호' type='password' value={user.pw} />
+                        <input id='nickname' placeholder='별명' type='text' value={user.nickname} />
+                        <select id='class' value={user.armyclass}>
                             <option>병장</option>
                             <option>상병</option>
                             <option>일병</option>
@@ -39,7 +44,7 @@ const Userinfo: React.FC = () => {
                         <button id='modifybutton' className='absolute'>
                             <HiOutlinePencilSquare className='w-8 h-8 p-1 rounded-full bg-orange-500 text-white' />
                         </button>
-                        <img src={profile_tmp} alt="profile_tmp" className='border-2 border-gray-100 rounded-full'/>
+                        <img src={profile_tmp} alt="profile_tmp" className='border-2 border-gray-100 rounded-full' />
                     </div>
 
                     <button className='block mx-auto mt-4 px-16 py-1 rounded-full bg-orange-500 text-center text-white'>수정</button>
@@ -51,3 +56,4 @@ const Userinfo: React.FC = () => {
 }
 
 export default Userinfo;
+
