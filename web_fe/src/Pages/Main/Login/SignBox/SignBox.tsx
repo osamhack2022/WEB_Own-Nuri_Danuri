80f4df295174e424d7becbe215da8ec7dd8bad2e
Goonco <64007080+Goonco@users.noreply.github.com>
2022-10-22 03:19:59 +0000

    post

--- web_fe/src/Pages/Main/Login/SignBox/SignBox.tsx
+++ web_fe/src/Pages/Main/Login/SignBox/SignBox.tsx
@@ -15,9 +15,10 @@ const SignBox: React.FC<props> = ({ certificate }) => {
 
     const onValid = (data: any) => {
         console.log("Valid!");
-        console.log(data);
 
-        axios.post('/UserInfo', data);
+        const { id, pw, armyunit } = data;
+        const defaultData = { id, name: 'default', armyunit, pw, nickname: 'default', armyclass: '이병' };
+        axios.post('/UserInfo', defaultData);
     };
 
     // 사용신청
@@ -27,9 +28,9 @@ const SignBox: React.FC<props> = ({ certificate }) => {
             <div id="SignBox_container" className='w-full overflow-hidden rounded-md text-center bg-white'>
                 <form onSubmit={handleSubmit(onValid)}>
 
-                    <input {...register('CertMilID')} placeholder='군번' type='text' className='inline-block my-4 border-b-2 w-3/5' />
-                    <input {...register('CertPW')} placeholder='비밀번호' type='password' className='inline-block my-4 border-b-2 w-3/5' />
-                    <input {...register('CertArmyUnit')} placeholder='부대' type='text' className='inline-block my-4 border-b-2 w-3/5' />
+                    <input {...register('id')} placeholder='군번' type='text' className='inline-block my-4 border-b-2 w-3/5' />
+                    <input {...register('pw')} placeholder='비밀번호' type='password' className='inline-block my-4 border-b-2 w-3/5' />
+                    <input {...register('armyunit')} placeholder='부대' type='text' className='inline-block my-4 border-b-2 w-3/5' />
 
                     <input type='submit' value='인증하기' className='block mx-auto my-4 w-1/5 border-2 rounded-full pt-1 border-blue-500 text-blue-500' />
 
@@ -44,8 +45,8 @@ const SignBox: React.FC<props> = ({ certificate }) => {
             <div id="SignBox_container" className='w-full overflow-hidden rounded-md text-center bg-white'>
                 <form>
 
-                    <input {...register('LoginMilID')} placeholder='군번' type='text' className='inline-block my-4 border-b-2 w-3/5' />
-                    <input {...register('LoginPW')} placeholder='비밀번호' type='password' className='inline-block my-4 border-b-2 w-3/5' />
+                    <input {...register('id')} placeholder='군번' type='text' className='inline-block my-4 border-b-2 w-3/5' />
+                    <input {...register('pw')} placeholder='비밀번호' type='password' className='inline-block my-4 border-b-2 w-3/5' />
 
                     <Link to="/dashboard/mostbenefits"><button className='block mx-auto my-4 w-1/5 border-2 rounded-full pt-1 border-blue-500 text-blue-500'>로그인</button></Link>
 
